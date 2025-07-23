2276e8eba1606c2e578c2dfce2ba15a182c1efb8
gouthami0610 <gouthamivemula2023@gmail.com>
2025-07-23 17:55:25 +0530

    Update app.py

--- mlops/api/app.py
+++ mlops/api/app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import requests
-mathmark=st.number_input("Enter math marks ")
-scimark=st.number_input("Enter sci marks ")
-engmark=st.number_input("Enter eng marks ")
-marks=[mathmark,scimark,engmark]
+
+mathmark = st.number_input("Enter math marks")
+scimark = st.number_input("Enter sci marks")
+engmark = st.number_input("Enter eng marks")
+marks = [mathmark, scimark, engmark]
+
 if st.button("Predict result"):
-    response = requests.post("https://localhost:8000/predict",json={"marks":marks})
-if response.status_code == 200:
-    result = response.json().get("prediction")
-    if result==1:
-        st.success("Passed")
+    response = requests.post("http://localhost:8000/predict", json={"marks": marks})
+    if response.status_code == 200:
+        result = response.json().get("prediction")
+        if result == 1:
+            st.success("Passed")
+        else:
+            st.error("Fail")
     else:
-        st.error("Fail")
+        st.error(f"API returned status code {response.status_code}")
